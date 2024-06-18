087abc0c76687325ea8620503d6989f13628261c
Yaswanth <yaswanthgodavarthi0006@gmail.com>
2024-06-18 21:02:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from 'react';
+import "./style.scss";
 
-function App() {
+
+export default function App() {
+
+    const[input,setInput] = useState([]);
+
+    const handler = e =>{
+        setInput(e.target.value);
+    }
+
+    const btnHandler = (e) => {
+        setInput(input+e.target.value)
+    } 
+
+    const clearHandler = () =>{
+        setInput("")
+    }
+
+    const evalHandler = () => {
+        setInput(eval(input))
+    }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+        <div className='inner-container'>
+            <div className='display'>
+                <input type="text" value={input} onChange={handler} onKeyDown={(e) => {
+        if (e.key === "Enter")
+            evalHandler();
+        }} />
+            </div>
+            <div className='buttons'>
+                <button className='button spl' onClick={btnHandler} value="(">(</button>
+                <button className='button spl' onClick={btnHandler} value=")">)</button>
+                <button className='button clear' onClick={clearHandler} >Clear</button>
+                <button className='button' onClick={btnHandler} value="1">1</button>
+                <button className='button' onClick={btnHandler} value="2">2</button>
+                <button className='button' onClick={btnHandler} value="3">3</button>
+                <button className='button spl' onClick={btnHandler} value="+">+</button>
+                <button className='button' onClick={btnHandler} value="4">4</button>
+                <button className='button' onClick={btnHandler} value="5">5</button>
+                <button className='button' onClick={btnHandler} value="6">6</button>
+                <button className='button spl' onClick={btnHandler} value="-">-</button>
+                <button className='button' onClick={btnHandler} value="7">7</button>
+                <button className='button' onClick={btnHandler} value="8">8</button>
+                <button className='button' onClick={btnHandler} value="9">9</button>
+                <button className='button spl' onClick={btnHandler} value="/">/</button>
+                <button className='button' onClick={btnHandler} value=".">.</button>
+                <button className='button' onClick={btnHandler} value="0">0</button>
+                <button className='button equalto' onClick={evalHandler} value="=">=</button>
+                <button className='button spl' onClick={btnHandler} value="*">X</button>
+            </div>
+        </div>
     </div>
-  );
+  )
 }
-
-export default App;
